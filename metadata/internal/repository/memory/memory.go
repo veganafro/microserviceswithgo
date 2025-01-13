08cce0f72642265a791a81c37395679371225e90
Jeremy Muhia <jmuhia80@gmail.com>
2025-01-13 10:16:46 -0500

    Add comments to metadata service

--- metadata/internal/repository/memory/memory.go
+++ metadata/internal/repository/memory/memory.go
@@ -7,17 +7,20 @@ import (
 	"sync"
 )
 
+// Repository defines an in-memory movie metadata repository.
 type Repository struct {
 	sync.RWMutex
 	data map[string]*model.Metadata
 }
 
+// New creates a new in-memory repository.
 func New() *Repository {
 	return &Repository{
 		data: map[string]*model.Metadata{},
 	}
 }
 
+// Get retrieves movie metadata using the provided movie id from the in-memory repository.
 func (r *Repository) Get(_ context.Context, id string) (*model.Metadata, error) {
 	r.RLock()
 	defer r.RUnlock()
@@ -28,6 +31,7 @@ func (r *Repository) Get(_ context.Context, id string) (*model.Metadata, error) 
 	return m, nil
 }
 
+// Put updates or creates movie metadata in the in-memory repository.
 func (r *Repository) Put(_ context.Context, id string, metadata *model.Metadata) error {
 	r.Lock()
 	defer r.Unlock()
--- metadata/pkg/model/metadata.go
+++ metadata/pkg/model/metadata.go
@@ -1,5 +1,6 @@
 package model
 
+// Metadata defines the movie metadata.
 type Metadata struct {
 	ID          string `json:"id"`
 	Title       string `json:"title"`
