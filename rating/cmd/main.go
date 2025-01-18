e77038affef5f66e8500232b08cf88c233811070
Jeremy Muhia <jmuhia80@gmail.com>
2025-01-18 13:25:19 -0500

    Register rating service on startup

--- rating/cmd/main.go
+++ rating/cmd/main.go
@@ -1,15 +1,48 @@
 package main
 
 import (
+	"context"
+	"flag"
+	"fmt"
 	"log/slog"
+	"movieexample.com/pkg/discovery"
+	"movieexample.com/pkg/discovery/consul"
 	"movieexample.com/rating/internal/controller/rating"
 	httphandler "movieexample.com/rating/internal/handler/http"
 	"movieexample.com/rating/internal/repository/memory"
 	"net/http"
+	"time"
 )
 
+const serviceName = "rating"
+
 func main() {
+	var port int
+	flag.IntVar(&port, "port", 8082, "API handler port")
+	flag.Parse()
 	slog.Info("starting rating service")
+	registry, err := consul.NewRegistry("localhost:8500")
+	if err != nil {
+		panic(err)
+	}
+
+	ctx := context.Background()
+	instanceID := discovery.GenerateInstanceID(serviceName)
+	err = registry.Register(ctx, instanceID, serviceName, fmt.Sprintf("localhost:%d", port))
+	if err != nil {
+		panic(err)
+	}
+
+	go func() {
+		for {
+			if err := registry.ReportHealthyState(context.Background(), instanceID, serviceName); err != nil {
+				slog.Error("failed to report healthy state", slog.String("err", err.Error()))
+			}
+			time.Sleep(1 * time.Second)
+		}
+	}()
+	defer registry.Deregister(ctx, instanceID, serviceName) // ignoring error
+
 	repo := memory.New()
 	ctrl := rating.New(repo)
 	handler := httphandler.New(ctrl)
